3968a36ffa851e3e6348fec3897559d8ed023ce8
Sergejs @Maruks <sergejs.maruks@gmail.com>
2017-09-25 13:18:25 +0200

    Added MainActivity. User Gets redirected to main activity if logged in

--- app/src/main/java/com/s_maruks/tutinava/eventgallery/LoginActivity.java
+++ app/src/main/java/com/s_maruks/tutinava/eventgallery/LoginActivity.java
@@ -3,12 +3,18 @@ package com.s_maruks.tutinava.eventgallery;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import static android.R.attr.value;
 
 
 /**
@@ -23,19 +29,41 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+
         boolean fbLogout = true;
         try{
             fbLogout = getIntent().getExtras().getBoolean("FBlogout");
         }catch (Exception e){
 
         }
+
         loginButton = (LoginButton)findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
+        //LoginManager.getInstance().logOut();
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+            }
+        };
+        // If the access token is available already assign it.
+        AccessToken  accessToken = AccessToken.getCurrentAccessToken();
+
+        if (accessToken!=null){
+            Intent new_activity = new Intent(LoginActivity.this, MainActivity.class);
+            //new_activity.putExtra("key", value);
+            LoginActivity.this.startActivity(new_activity);
+        }
         try{
             loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                 @Override
                 public void onSuccess(LoginResult loginResult) {
-
+                    Intent new_activity = new Intent(LoginActivity.this, MainActivity.class);
+                    //new_activity.putExtra("key", value);
+                    LoginActivity.this.startActivity(new_activity);
                 }
 
                 @Override
